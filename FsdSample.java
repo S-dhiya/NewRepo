8864f645f32fe208f06afd61535afcf0923a51fe
S-dhiya <137158283+S-dhiya@users.noreply.github.com>
2023-06-21 13:01:51 +0530

    change the code

--- FsdSample.java
+++ FsdSample.java
@@ -4,7 +4,7 @@
       int b=64;
        int c=a+b;
         for(int i=0;i<=5;i++)
-       System.out.println("sandhiya");
+       System.out.println("hi friends");
       System.out.println(c);
      }
      }
