be70ead84afd1b62b3e709cbe886053bd51cd754
S-dhiya <137158283+S-dhiya@users.noreply.github.com>
2023-06-21 12:51:14 +0530

    update some code

--- FsdSample.java
+++ FsdSample.java
@@ -1,9 +1,9 @@
   public class NewRepo{
     public static void main(String args[]){
-      int a=3;
+      int a=45;
       int b=64;
        int c=a+b;
        System.out.println("sandhiya");
-      
+      System.out.println(c);
      }
      }
